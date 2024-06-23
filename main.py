c26281b32fbed017c58bf7db9312d7cc02e0dac9
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-06-23 20:27:10 +0530

    Updated gunicorn start commond with adding Procfile

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ app = register_routes(app)
 
 if __name__ == "__main__":
     # app.run(port=5001, debug=True)
-    #app.run(host="0.0.0.0", port=5001, debug=True)
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host="0.0.0.0", port=5001, debug=True)
+    #app.run(debug=True, port=os.getenv("PORT", default=5000))
