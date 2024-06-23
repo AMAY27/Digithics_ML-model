4df792211949245c361b3c640cbc7f406284d8c7
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-06-23 19:02:16 +0530

    Updated the port according to railway configuration

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from routes import register_routes
-
+import os
 app = Flask(__name__)
 
 app = register_routes(app)
 
 if __name__ == "__main__":
     # app.run(port=5001, debug=True)
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run( port=os.getenv("PORT",default=5001), debug=True)
